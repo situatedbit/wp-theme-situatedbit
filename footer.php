161c9068734c31178e3684e392728ceca9bd1232
Matt Schaefer <matt@situatedbit.com>
2015-02-17 22:52:51 -0800

    re-arranged footer to include portrait; portrait still missing

--- footer.php
+++ footer.php
@@ -2,8 +2,13 @@
     </div> <!-- content -->
     <footer id="footer">
       <div class="footer-wrapper constrained-layout">
-        <a id="footer-logo" href="/">Situated Bit Home</a>
         <?php sb_footer_menu(); ?>
+        <div id="footer-bio-wrapper">
+          <p id="footer-bio">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue. 
+          </p>
+          <img id="footer-portrait" src="">
+        </div>
         <p class="copyright">&copy; Matt Schaefer</p>
       </div>
     </footer> <!-- #footer -->
