51cd187c3120b38bd98d691bc90938101050e47f
Matt Schaefer <matt@situatedbit.com>
2015-02-22 15:55:51 -0800

    fixed footer widget for bio; did not expect list items; ugh

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
       <div class="footer-wrapper constrained-layout">
         <?php sb_footer_menu(); ?>
         <div id="footer-bio-wrapper">
-          <p id="footer-bio">
+          <ul id="footer-bio">
             <?php sb_footer_bio_sidebar(); ?>
-          </p>
+          </ul>
           <img id="footer-portrait" src="<?php echo get_template_directory_uri() . '/images/matt-schaefer-portrait.jpg'; ?>" alt="Matt Schaefer portrait at the Gamble House, Pasadena, Calif.">
 
         </div>
