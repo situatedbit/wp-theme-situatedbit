cb6f727b35780255ff93b1f71196b00d2b6efd70
Matt Schaefer <matt@situatedbit.com>
2015-02-22 13:26:21 -0800

    added sorts mill

--- post-footer.php
+++ post-footer.php
@@ -1,4 +1,4 @@
-<footer class="post-footer">
+<footer class="post-footer post-meta">
   <?php the_tags('<ul class="tags"><li>', '</li><li>', '</li></ul>') ?>
 
 	<?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?>
