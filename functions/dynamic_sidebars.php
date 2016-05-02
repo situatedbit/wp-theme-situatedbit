4fa4326f230391201bc699dc61554919d4127062
Matt Schaefer <matt@situatedbit.com>
2016-05-02 11:51:05 +0900

    fixed call to register sidebar

--- functions/dynamic_sidebars.php
+++ functions/dynamic_sidebars.php
@@ -1,7 +1,7 @@
 <?php
   if ( function_exists('register_sidebar') ) {
-    register_sidebar( array('name' => 'Hidden Scripts') );
-    register_sidebar( array('name' => 'Footer Bio') );
+    register_sidebar( array('id' => 'situatedbit-hidden-scripts', 'name' => 'Hidden Scripts') );
+    register_sidebar( array('id' => 'situatedbit-footer-bio', 'name' => 'Footer Bio') );
   }
 
   function sb_hidden_scripts_sidebar() {
